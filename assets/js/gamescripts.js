f127dc9246befecdb191556114bb2e232c77b119
ChrisCODES23 <christiantalledo89@gmail.com>
2022-09-02 12:03:42 -0700

    reference for fetch

--- assets/js/gamescripts.js
+++ assets/js/gamescripts.js
@@ -20,80 +20,80 @@ submitBtn.on('click', '.dropdown-item', function () {
 
 // function to call games 
 
-function getGames() {
-    var platform = ("#platform")
-    var genre = $("#genre")
-    var image = $("#image")
-    var game = $("#game")
-    var gameUrl = "https://cors-anywhere.herokuapp.com/https://api.rawg.io/api/genres?key=0da6d52b21ec4d8fac88f4f4ceafe806"
-    var platformUrl = "https://cors-anywhere.herokuapp.com/https://api.rawg.io/api/platforms?key=0da6d52b21ec4d8fac88f4f4ceafe806"
+// function getGames() {
+//     var platform = ("#platform")
+//     var genre = $("#genre")
+//     var image = $("#image")
+//     var game = $("#game")
+//     var gameUrl = "https://cors-anywhere.herokuapp.com/https://api.rawg.io/api/genres?key=0da6d52b21ec4d8fac88f4f4ceafe806"
+//     var platformUrl = "https://cors-anywhere.herokuapp.com/https://api.rawg.io/api/platforms?key=0da6d52b21ec4d8fac88f4f4ceafe806"
 
 
 
-    fetch(platformUrl)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            console.log(data.results.name);
-            var platforms = data.results.name
-            console.log(platforms);
-            $(platform).attr('data-value',)
-        })
+//     fetch(platformUrl)
+//         .then(function (response) {
+//             if (response.ok) {
+//                 return response.json();
+//             }
+//         })
+//         .then(function (data) {
+//             console.log(data.results.name);
+//             var platforms = data.results.name
+//             console.log(platforms);
+//             $(platform).attr('data-value',)
+//         })
 
 
-    fetch(gameUrl)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            console.log(data.results.name);
-            var genres = data.results.name
-            console.log(genres);
-            $(genre).attr('data-value',)
-        })
+//     fetch(gameUrl)
+//         .then(function (response) {
+//             if (response.ok) {
+//                 return response.json();
+//             }
+//         })
+//         .then(function (data) {
+//             console.log(data.results.name);
+//             var genres = data.results.name
+//             console.log(genres);
+//             $(genre).attr('data-value',)
+//         })
 
-    fetch(gameUrl)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            console.log(data.results.image_background);
-            var img = data.results.image_background
-            console.log(img);
-            $(image).attr('data-value',)
-        })
-
-
-    fetch(gameUrl)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            console.log(data.results.games.name);
-            var games = data.results.games.name
-            console.log(games);
-            $(game).attr('data-value',)
-            // 3 games
-        });
-
-    getGames();
+//     fetch(gameUrl)
+//         .then(function (response) {
+//             if (response.ok) {
+//                 return response.json();
+//             }
+//         })
+//         .then(function (data) {
+//             console.log(data.results.image_background);
+//             var img = data.results.image_background
+//             console.log(img);
+//             $(image).attr('data-value',)
+//         })
 
 
+//     fetch(gameUrl)
+//         .then(function (response) {
+//             if (response.ok) {
+//                 return response.json();
+//             }
+//         })
+//         .then(function (data) {
+//             console.log(data.results.games.name);
+//             var games = data.results.games.name
+//             console.log(games);
+//             $(game).attr('data-value',)
+//             // 3 games
+//         });
 
+//     getGames();
 
 
 
 
-}
+
+
+
+// }
 
 
 
