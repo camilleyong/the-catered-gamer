ec08fff970bc49f400fa42852785bd686a4dfd6f
ChrisCODES23 <christiantalledo89@gmail.com>
2022-09-09 10:45:15 -0700

    added trailer to the search query

--- assets/js/gamescripts.js
+++ assets/js/gamescripts.js
@@ -12,7 +12,7 @@ console.log(game)
 //function for youtube videos
 function getYouTube(game) {
     var youTubeVideo = $("#youTubeVideo");
-    var trailerUrl = "https://cors-anywhere.herokuapp.com/https://serpapi.com/search.json?engine=youtube&search_query=" + game + "&api_key=58f4d32ec9b9734f0935989c9def9f0766c97c4092a3e8b56d00745a828c4eb1";
+    var trailerUrl = "https://cors-anywhere.herokuapp.com/https://serpapi.com/search.json?engine=youtube&search_query=" + game + "trailersp=EgJwAQ%3D%3D&api_key=58f4d32ec9b9734f0935989c9def9f0766c97c4092a3e8b56d00745a828c4eb1";
 
 
     fetch(trailerUrl)
